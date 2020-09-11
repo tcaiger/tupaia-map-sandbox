5d94e99a63d699e1056acb872c8a9ee8af17917c
Tom Caiger <caigertom@gmail.com>
2020-09-11 16:21:29 +1200

    Updates

---
+++ precache-manifest.29b7728a29b48adcceaa76d154282090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff37280870634f3f3f22502b9bd6045f",
+    "url": "/tupaia-map-sandbox/index.html"
+  },
+  {
+    "revision": "925d0e6a172457a8c82e",
+    "url": "/tupaia-map-sandbox/static/js/2.7cf98a11.chunk.js"
+  },
+  {
+    "revision": "8819d044fc9cdbcc358fde839f0afde6",
+    "url": "/tupaia-map-sandbox/static/js/2.7cf98a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e8a90a52f835564789",
+    "url": "/tupaia-map-sandbox/static/js/main.7a92dd4e.chunk.js"
+  },
+  {
+    "revision": "79152c6d176b8e076027",
+    "url": "/tupaia-map-sandbox/static/js/runtime-main.72b9ba2b.js"
+  }
+]);
